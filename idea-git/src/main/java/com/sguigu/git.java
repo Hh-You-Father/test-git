da296f6acbffa4eb28e155c9c628ed49d71a6a8d
hh <hh@qq.com>
2021-12-19 20:36:54 +0800

    21 12 19 20 36 idea

--- idea-git/src/main/java/com/sguigu/git.java
+++ idea-git/src/main/java/com/sguigu/git.java
@@ -2,6 +2,10 @@ package com.sguigu;
 
 public class git {
     public void test01(){
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+    }
+
+    public void test02(){
+        System.out.println("Hello Git2");
     }
 }
